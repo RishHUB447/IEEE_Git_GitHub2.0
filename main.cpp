d119bd89329a883cef47777fee593604735f2368
RishHUB447 <rishabh447verma@gmail.com>
2026-02-08 22:00:48 +0530

    f2

--- main.cpp
+++ main.cpp
@@ -3,6 +3,10 @@
 
 using namespace std;
 
+int sum(int a, int b){
+    return a +b;
+}
+
 int main(){
     string name;
     cout << "Enter your name: ";
@@ -12,5 +16,9 @@ int main(){
     for (const char c : name){
         cout << c << " ";
     }
+
+    int j = 0;
+    while (j < 100) j++;
+    cout << "\nLoop finished, j = " << j << endl;
     return 0;
 }
